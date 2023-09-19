6af6ffef2924acb6d7fd2d5bc01fc0774378ed33
BeyenaAlhinai <Beyenaalhinai@gmail.com>
2023-09-19 13:33:47 +0400

    ToDoList

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'todolist',component:TodolistComponent},
+  {path:'',component:AppComponent}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
